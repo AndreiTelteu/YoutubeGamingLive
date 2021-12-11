98fd74ee05b2d623248569dda75f7ff7a31166a1
Andrei Telteu <andrei@telteu.ro>
2021-12-11 20:18:48 +0000

    Login with youtube

--- config/services.php
+++ config/services.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Third Party Services
@@ -14,20 +13,25 @@ return [
     |
     */
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    "youtube" => [
+        "client_id" => env("YOUTUBE_CLIENT_ID"),
+        "client_secret" => env("YOUTUBE_CLIENT_SECRET"),
+        "redirect" => env("YOUTUBE_REDIRECT_URI"),
     ],
 
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
+    "mailgun" => [
+        "domain" => env("MAILGUN_DOMAIN"),
+        "secret" => env("MAILGUN_SECRET"),
+        "endpoint" => env("MAILGUN_ENDPOINT", "api.mailgun.net"),
     ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    "postmark" => [
+        "token" => env("POSTMARK_TOKEN"),
     ],
 
+    "ses" => [
+        "key" => env("AWS_ACCESS_KEY_ID"),
+        "secret" => env("AWS_SECRET_ACCESS_KEY"),
+        "region" => env("AWS_DEFAULT_REGION", "us-east-1"),
+    ],
 ];
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::get("/youtube/login", function () {
+    return \Socialite::driver("youtube")
+        ->stateless()
+        ->redirect();
+});
+
+Route::get("/youtube/callback", function () {
+    $user = \Socialite::driver("youtube")
+        ->stateless()
+        ->user();
+    dd([
+        "id" => $user->id,
+        "name" => $user->nickname,
+        "avatar" => $user->avatar,
+        "all" => $user,
+    ]);
 });
