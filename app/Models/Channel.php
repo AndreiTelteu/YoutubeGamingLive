a6811f62756c6d3a71b71cbbe456c23bfb50de8f
Andrei Telteu <andrei@telteu.ro>
2021-12-24 19:07:35 +0000

    Channel function to check if a channel is online

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Events\ChannelCreated;
+use Youtube;
 
 class Channel extends Model
 {
@@ -37,5 +38,30 @@ class Channel extends Model
             return null;
         }
         return self::where("youtube_id", $channel_id)->first();
+    }
+
+    public function checkIfLive()
+    {
+        $params = [
+            "channelId" => $this->youtube_id,
+            "part" => "snippet",
+            "type" => "channel",
+            "maxResults" => 1,
+        ];
+        $data = json_decode(
+            Youtube::api_get(
+                "https://youtube.googleapis.com/youtube/v3/search",
+                $params
+            )
+        );
+        if (
+            $data &&
+            $data->items &&
+            $data->items[0] &&
+            $data->items[0]->snippet->liveBroadcastContent == "live"
+        ) {
+            return true;
+        }
+        return false;
     }
 }
