8e11648a6a7860402516bbb47e2abc9a3c028474
Andrei Telteu <andrei@telteu.ro>
2022-01-23 01:40:08 +0000

    Installed soketi with pm2 cluster, and nginx proxy

---
+++ soketi/ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [
+        {
+            name: "soketi",
+            cwd: "/www/wwwroot/youtubegaming.live/soketi/",
+            script: "soketi",
+            args: [
+                "start",
+                "--config=/www/wwwroot/youtubegaming.live/soketi/config.json",
+            ],
+            watch: false,
+            // interpreter: "/home/ubuntu/.fnm/fnm",
+            // interpreter_args: "exec --using=14 node",
+            instances: 4,
+            exec_mode: "cluster",
+            autorestart: true,
+        },
+    ],
+};
