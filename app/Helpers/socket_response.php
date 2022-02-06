9e5e8f60357ab946b5a7474d17c1e499f0c5f735
Andrei Telteu <andrei@telteu.ro>
2022-02-06 13:29:07 +0000

    Removed laravel/swoole plugin and files

--- app/Helpers/socket_response.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!function_exists("socket_response")) {
-    function socket_response($data)
-    {
-        return base64_encode(json_encode($data));
-    }
-}
--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WsController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ChannelController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Broadcast::routes(["middleware" => ["auth:api"]]);
 
