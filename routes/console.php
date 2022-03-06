ea308c50b741026fde6946570524b3fab078bac3
Andrei Telteu <andrei@telteu.ro>
2022-03-06 12:12:26 +0000

    docker network to publish to nginx proxy man, test email command

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,16 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('test:email {email?}', function ($email = 'aandreyy96@gmail.com') {
+    try {
+        $sent = \Mail::send([], [], function ($message) use ($email) {
+            $message->to($email)
+                ->subject('test email from laravel')
+                ->setBody(" test dada email", 'text/html');
+        });
+        $this->info('Email sent successfully');
+    } catch (\Exception $e) {
+        $this->error($e->getMessage());
+    }
+})->purpose('test email');
